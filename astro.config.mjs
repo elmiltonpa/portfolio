d7ca7840299fce4b3012cc37ed1370598ed2611c
Milton Ibarra <elmiltonibarra@gmail.com>
2026-04-21 20:40:31 -0300

    add vercel analytics integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
+import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
   },
+ adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
